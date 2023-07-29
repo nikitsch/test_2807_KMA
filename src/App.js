6de66607d3288bd237e37c0183f9a7c1a38ada2c
nikitsch <nikitsch5@gmail.com>
2023-07-29 22:24:11 +0300

    fix: click processing logic

--- src/App.js
+++ src/App.js
@@ -1,65 +1,93 @@
 import { useState } from 'react';
 import { Element } from './Element';
 import style from './App.module.css';
+import { v1 } from 'uuid';
+
+const arr = [
+  { "value": "[1]", "blok": "925115218", "colorBlok": "#FF4715", "isClick": false },
+  { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
+  { "value": "[2]", "blok": "512310252", "colorBlok": "#FF1020", "isClick": false },
+  { "value": "[2]", "blok": "512310252", "colorBlok": "#FF1020", "isClick": false },
+  { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
+  { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
+  { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
+  { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
+  { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
+  { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
+]
 
 function App() {
-
-  const arr = [
-    { "value": "1", "blok": "1", "colorBlok": "#FF4715", "isClick": false },
-    { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
-    { "value": "2", "blok": "22", "colorBlok": "#FF1020", "isClick": false },
-    { "value": "2", "blok": "22", "colorBlok": "#FF1020", "isClick": false },
-    { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
-    { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
-    { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
-    { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
-    { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
-    { "value": "_", "blok": "empty", "colorBlok": "#D0D0D0", "isClick": false },
-  ]
   const [objArr, setValue] = useState(arr);
   const [text, setText] = useState('');
 
   function addBlok(enter) {
 
     const color = '#F' + (Math.random().toString(16) + '00000').substring(3, 8).toUpperCase()
+    const idBlock = v1()
 
-    const arrEnter = enter.split("")
-    arrEnter.map(el => {
-      const underscore = objArr.find(emptyField => emptyField.value === "_")
-      underscore.value = el
-      underscore.blok = enter
-      underscore.colorBlok = color
-      return el
+    enter.split('').forEach(symbol => {
+      let pasted = false;
+
+      setValue(arr => arr.map(el => {
+        if (el.value === '_' && !pasted) {
+          pasted = true;
+          return {
+            ...el,
+            value: "[" + symbol + "]",
+            blok: idBlock,
+            colorBlok: color,
+          }
+        }
+
+        return el;
+      }))
     })
   }
 
 
-  // let bracket
-  // const arrNew = arr.map(p => {
-
-  //   if (p.blok === bracket) {
-  //     bracket = p.blok
-  //     return { ...p }
-  //   } else {
-  //     bracket = p.blok
-  //     return { ...p, value: "|" + p.value }
-  //   }
-  // })
-  // console.log(arrNew.map(e => e.value).reduce((prev, curr) => prev + curr))
 
   const handleAction = () => {
+    const emptyFields = objArr.filter(el => el.value === "_").length
+    if (text.trim().length > emptyFields) {
+      alert('Нет места. Удалите какой-либо блок')
+      return
+    }
     if (text.trim().length) {
-
       addBlok(text.trim())
       setText('');
     }
-    // console.log(arr);
   }
 
   const handleSubmit = (e) => {
     setText(e.target.value)
   }
-  // console.log("objArr:", objArr);
+
+  const singleClick = (group) => {
+    setValue((symbols) => symbols.map(({ isClick, ...props }) => ({
+      ...props,
+      isClick: group === props.blok && props.value !== '_' ? !isClick : false,
+    })))
+  }
+
+  const doubleClick = (group) => {
+    setValue((symbols) => symbols.map((el) => group === el.blok ? ({
+      ...el,
+      value: '_',
+      blok: 'empty',
+      colorBlok: "#D0D0D0",
+      isClick: false
+    }) : el))
+  }
+
+  // for (let i = 0; i < objArr.length; i++) {
+  //   if (objArr[i].value === "_") continue
+  //   if (objArr[i].blok === objArr[i + 1].blok) {
+  //     objArr[i].value = objArr[i].value.slice(0, 2)
+  //     objArr[i + 1].value = objArr[i + 1].value.slice(1)
+  //   }
+
+  // }
+
   return (
     <>
       <div>
@@ -74,7 +102,14 @@ function App() {
       </div>
       <div className={style.app}>
         {objArr.map((el) => {
-          return <Element sign={el.value} group={el.blok} color={el.colorBlok} />
+          return <Element
+            sign={el.value}
+            group={el.blok}
+            color={el.colorBlok}
+            isClick={el.isClick}
+            singleClick={singleClick}
+            doubleClick={doubleClick}
+          />
         })}
       </div>
     </>
--- src/Element.jsx
+++ src/Element.jsx
@@ -1,8 +1,23 @@
+import { useCallback } from 'react';
 import style from './Element.module.css';
 
-export const Element = ({el}) => {
+export const Element = ({ sign, group, color, isClick, singleClick, doubleClick }) => {
+
+  const styleSingleClick = isClick ? "scale(1.2)" : "scale(1)"
+
+  const handleClick = useCallback(() => {
+    singleClick(group);
+  }, [group])
+
+  const handleDoubleClick = useCallback(() => {
+    doubleClick(group);
+  }, [group])
 
   return (
-    <div className={style.element}>{el}</div>
+    <div className={style.element}
+      style={{ background: color, transform: styleSingleClick }}
+      onClick={handleClick}
+      onDoubleClick={handleDoubleClick}
+    >{sign}</div>
   )
 }
